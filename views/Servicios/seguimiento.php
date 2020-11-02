19eca93e62560fe0f6f0be0b5659a0e0827b7f03
car121-stack <carlosrod061@gmail.com>
2020-11-02 12:35:17 -0600

    Seguimiento
    Cambios de seguimiento.

---
+++ views/Servicios/seguimiento.php
@@ -0,0 +1,308 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="utf-8" />
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+  <meta name="description" content="" />
+  <meta name="author" content="" />
+  <title>UTNG</title>
+  <!-- Favicon-->
+  <link rel="icon" type="image/x-icon" href="../../assets/img/favicon.ico" />
+  <!-- Font Awesome icons (free version)-->
+  <script src="https://use.fontawesome.com/releases/v5.13.0/js/all.js" crossorigin="anonymous"></script>
+  <!-- Google fonts-->
+  <link href="https://fonts.googleapis.com/css?family=Montserrat:400,700" rel="stylesheet" type="text/css" />
+  <link href="https://fonts.googleapis.com/css?family=Lato:400,700,400italic,700italic" rel="stylesheet"
+    type="text/css" />
+  <!-- Core theme CSS (includes Bootstrap)-->
+  <link href="../../css/styles.css" rel="stylesheet" />
+  <link href="../../css/servicios.css" rel="stylesheet" />
+  <link href="../../css/servicios/seguimiento.css" rel="stylesheet" />
+</head>
+
+<body id="page-top">
+  <!-- Navigation-->
+
+  <?php include '../Plantillas/header.php';?>
+
+  <!-- Masthead-->
+  <header class="masthead text-white">
+    <div class="container">
+
+      <div class="card" id="tituloServicio">
+        <div class="card-body">
+          <h4>
+            Bolsa de Trabajo
+          </h4>
+        </div>
+      </div>
+
+      <!--CARROUSEL CON IMAGENES-->
+      <div id="carouselExampleIndicators" class="carousel slide col-md-8 mx-auto" data-ride="carousel">
+        <ol class="carousel-indicators">
+          <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
+          <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
+          <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
+        </ol>
+        <div class="carousel-inner">
+          <div class="carousel-item active">
+            <img class="d-block w-100" src="../../assets/img/GaleriaServicios/seguimiento/bolsabanner.jpg" alt="First slide" height="350">
+          </div>
+        </div>
+        <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
+          <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+          <span class="sr-only">Previous</span>
+        </a>
+        <a class="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
+          <span class="carousel-control-next-icon" aria-hidden="true"></span>
+          <span class="sr-only">Next</span>
+        </a>
+      </div>
+
+      <!-- CONTENIDO RELEVANTE DE LA SECCION-->
+      <div id="accordion">
+        <div class="card">
+          <div class="card-header" id="headingOne">
+            <h5 class="mb-0">
+              <button class="btn btn-link" data-toggle="collapse" data-target="#collapseOne" aria-expanded="true"
+                aria-controls="collapseOne">
+                Descripción
+              </button>
+            </h5>
+          </div>
+
+          <div id="collapseOne" class="collapse show" aria-labelledby="headingOne" data-parent="#accordion">
+            <div class="card-body">
+              <p class="text-justify">
+               <b>En el área de Seguimiento de Egresados y bolsa de trabajo damos seguimiento a los egresados para obtener información sobre:</b> 
+                    <br>
+
+                <ol>
+                <li>  La satisfacción de los servicios que te brindamos.</li>
+                <li>    El nivel de exigencia de los empleadores en el desempeño profesional de los egresados.</li>
+                <li>
+                Realizar detecciones de necesidades de capacitación en tu ámbito profesional y ofrecerte cursos y talleres.</li>
+                <li>  Conocer tu trayectoria laboral.</li>
+              
+            
+              
+                </ol>
+              
+                A través de los siguientes estudios:
+<br>
+<br>
+                <div class="form-group row mt-2 mb-2">
+                    <div class="col-sm-3 text-center magic-top" style="opacity: 1; position: relative; top: 0px;">
+                    <img src="https://www.utng.edu.mx/assets/principal/servicios/bolsa/num1.png" width="160em" class="rounded-circle">
+                        <!--  <div class="circulo">
+                            <h1 class="text-center text-white numCir">1</h1>
+                        </div>-->
+                        <p>
+                            <strong>Colocación en el primer empleo <br> después de
+                                egresar y satisfacción de los servicios recibidos.
+                            </strong>
+                        </p>
+                    </div>
+                    <div class="col-sm-3 text-center magic-top" style="opacity: 1; position: relative; top: 0px;">
+                    <img src="https://www.utng.edu.mx/assets/principal/servicios/bolsa/num2.png" width="160em" class="rounded-circle">
+                        <!-- <div class="circulo align-middle">
+                            <h1 class="text-center  text-white numCir">2</h1>
+                        </div> -->
+                        <p>
+                            <strong>Seguimiento a egresados<br> y detección de necesidades<br>
+                                de capacitación.
+                            </strong>
+                        </p>
+                    </div>
+                    <div class="col-sm-3 text-center magic-top" style="opacity: 1; position: relative; top: 0px;">
+                    <img src="https://www.utng.edu.mx/assets/principal/servicios/bolsa/num3.png" width="160em" class="rounded-circle">
+                        <!-- <div class="circulo">
+                            <h1 class="text-center text-white numCir">3</h1>
+                        </div> -->
+                        <p>
+                            <strong>Estudio de trayectoria <br> laboral a dos años <br>de
+                                egreso.
+                            </strong>
+                        </p>
+                    </div>
+                    <div class="col-sm-3 text-center magic-top" style="opacity: 1; position: relative; top: 0px;">
+                    <img src="https://www.utng.edu.mx/assets/principal/servicios/bolsa/num4.png" width="160em" class="rounded-circle">
+                        <!-- 	<div class="circulo">
+                            <h1 class="text-center text-white numCir">4</h1>
+                        </div> -->
+                        <p>
+                            <strong>Estudio<br> de&nbsp;satisfacción<br> de empleadores.
+                            </strong>
+                        </p>
+                    </div>
+
+                    <br> <br> <br> <br>
+                    <p>
+                        &nbsp;&nbsp;Esto nos permitirá generar propuestas de mejora en nuestros <b>planes
+                            y programas de estudio.</b>
+                    </p>
+                </div>
+
+              </p>
+            </div>
+          </div>
+        </div>
+        <div class="card">
+          <div class="card-header" id="headingTwo">
+            <h5 class="mb-0">
+              <button class="btn btn-link collapsed" data-toggle="collapse" data-target="#collapseTwo"
+                aria-expanded="false" aria-controls="collapseTwo">
+                Servicios
+              </button>
+            </h5>
+          </div>
+          <div id="collapseTwo" class="collapse" aria-labelledby="headingTwo" data-parent="#accordion">
+            <div class="card-body">
+              <ol class="list-group">
+                <li class="list-group-item">Asesoría:
+                    <ul>
+                        <li>Talleres para buscadores activos de empleo</li>
+                        <li>Revisión de CV Tip’s para búsqueda de empleo</li>
+                        <li>Bolsas de trabajo en línea</li>
+                        <li>Difusión de programas de apoyo al autoempleo</li>
+                    </ul>
+                </li>
+                <li class="list-group-item">Apoyo para la postulación de vacantes registradas en BT.</li>
+
+              </ol>
+            </div>
+          </div>
+        </div>
+        <div class="card">
+          <div class="card-header" id="headingThree">
+            <h5 class="mb-0">
+              <button class="btn btn-link collapsed" data-toggle="collapse" data-target="#collapseThree"
+                aria-expanded="false" aria-controls="collapseThree">
+                Políticas y Requisitos para Bolsa de Trabajo
+              </button>
+            </h5>
+          </div>
+          <div id="collapseThree" class="collapse" aria-labelledby="headingThree" data-parent="#accordion">
+            <div class="card-body">
+              <ul class="list-group">
+                <li class="list-group-item">* Realizar el registro de manera presencial o vía correo electrónico, llevar tu CV y una fotografía tamaño infantil reciente.</li>
+                <li class="list-group-item">* Acudir a las actividades convocadas por Bolsa de Trabajo y Seguimiento de Egresados.</li>
+                <li class="list-group-item">* Tener disponibilidad para trabajar tiempo completo</li>
+                <li class="list-group-item">* Tener disponibilidad para establecerse en el lugar de la vacante si ésta así lo requiere</li>
+              </ul>
+            </div>
+          </div>
+        </div>
+        <div class="card">
+          <div class="card-header" id="headingFour">
+            <h5 class="mb-0">
+              <button class="btn btn-link collapsed" data-toggle="collapse" data-target="#collapseFour"
+                aria-expanded="false" aria-controls="collapseThree">
+                Bolsas de Trabajo
+              </button>
+            </h5>
+          </div>
+          <div id="collapseFour" class="collapse" aria-labelledby="headingFour" data-parent="#accordion">
+            <div class="card-body">
+             <ul class="list-group">
+                <li class="list-group-item avatar"><img src="../../assets/img/GaleriaServicios/seguimiento/occmundial.jpg" width=50 height=50 class="imgCir"> <span class="title">Occmundial</span> <a href="https://www.occ.com.mx/" class="secondary-content"> <i class="fas fa-info-circle"></i></a></li>
+                <li class="list-group-item avatar"><img src="../../assets/img/GaleriaServicios/seguimiento/sne.png"width=50 height=50 class="imgCir"> <span class="title">Servicio Nacional de
+                        Empleo</span> <a href="https://www.empleo.gob.mx/swb/empleo" class="secondary-content"><i class="fas fa-info-circle"></i></a></li>
+                <li class="list-group-item avatar"><img src="../../assets/img/GaleriaServicios/seguimiento/computrabajo.png" width=50 height=50 class="imgCir"> <span class="title">Computrabajo</span> <a href="http://www.computrabajo.com.mx/" class="secondary-content"> <i class="fas fa-info-circle"></i></a></li>
+                <li class="list-group-item avatar"><img src="../../assets/img/GaleriaServicios/seguimiento/bumeran.jpg" width=50 height=50 class="imgCir"> <span class="title">Bumeran</span> <a href="http://www.bumeran.com.mx/empleos-en-guanajuato.html" class="secondary-content"><i class="fas fa-info-circle"></i></a></li>
+                <li class="list-group-item avatar"><img src="../../assets/img/GaleriaServicios/seguimiento/jobrapido.jpg" width=50 height=50 class="imgCir"> <span class="title">JobRapido</span> <a href="http://mx.jobrapido.com/" class="secondary-content"> <i class="fas fa-info-circle"></i></a></li>
+                <li class="list-group-item avatar"><img src="../../assets/img/GaleriaServicios/seguimiento/jobisjob.png" width=50 height=50 class="imgCir"> <span class="title">JobisJob</span> <a href="https://www.jobisjob.com.mx/" class="secondary-content"> <i class="fas fa-info-circle"></i></a></li>
+                <li class="list-group-item avatar"><img src="../../assets/img/GaleriaServicios/seguimiento/mitula.jpg" width=50 height=50 class="imgCir"> <span class="title">Mitula</span> <a href="www.mitula.com" class="secondary-content"> <i class="fas fa-info-circle"></i></a></li>
+	    	</ul>
+            </div>
+          </div>
+        </div>
+        <div class="card">
+          <div class="card-header" id="headingFive">
+            <h5 class="mb-0">
+              <button class="btn btn-link collapsed" data-toggle="collapse" data-target="#collapseFive"
+                aria-expanded="false" aria-controls="collapseThree">
+                Link's de interés
+              </button>
+            </h5>
+          </div>
+          <div id="collapseFive" class="collapse" aria-labelledby="headingFive" data-parent="#accordion">
+            <div class="card-body">
+                <ul class="list-group">
+                        <li class="list-group-item"><a href="http://blog.occ.com.mx/el-tamano-si-importa-7-tips-para-reducir-la-extension-de-tu-cv/#.WO6Xu_k1_ce">
+                                Consejos para reducir tu CV y hacerlo más atractivo.</a></li>
+                        <li class="list-group-item"><a href="http://blog.occ.com.mx/cosas-en-tu-cv-que-dicen-no-me-contrates/#.WO6ZVPnhDcc">Lo
+                                que debes corregir en tu CV ahora mismo</a></li>
+                        <li class="list-group-item"><a href="http://blog.occ.com.mx/redes-sociales-en-tu-cv-si-o-no/#.WO6ZJfnhDcc">Redes
+                                sociales en tu CV ¿Si o No?</a></li>
+                        <li class="list-group-item"><a href="http://blog.occ.com.mx/preparate-para-tu-primera-entrevista-de-trabajo-del-2016/#.WO6ZK_nhDcc">Prepárate
+                                para tu primera entrevista laboral</a></li>
+                        <li class="list-group-item"><a href="http://blog.occ.com.mx/preparate-para-brillar-en-una-entrevista-de-panel/#.WO6ZjvnhDcc">Prepárate
+                                para una entrevista de panel</a></li>
+                        <li class="list-group-item"><a href="http://www.elfinanciero.com.mx/power-tools/las-10-preguntas-mas-dificiles-de-una-entrevista-laboral.html">Las
+                                10 preguntas más difíciles de una entrevista.</a></li>
+                        <li class="list-group-item"><a href="http://blog.occ.com.mx/7-dudas-sobre-los-examenes-psicometricos/#.WO6ZyvnhDcc">7
+                                dudas acerca de los exámenes psicométricos</a></li>
+                        <li class="list-group-item"><a href="http://blog.occ.com.mx/trabajo-voluntario-el-plus-que-tu-perfil-profesional-necesita/#.WO6ZL_nhDcc">Trabajo
+                                voluntario: el plus que tu perfil profesional necesita</a></li>
+                        <li class="list-group-item"><a href="http://blog.occ.com.mx/consejos-reales-para-tu-primer-trabajo/#.WO6ZN_nhDcc">Consejos
+                                para tu primer trabajo</a></li>
+                        <li class="list-group-item"><a href=" http://blog.occ.com.mx/90-de-los-mexicanos-estan-de-acuerdo-con-estudiar-y-trabajar-al-mismo-tiempo/#.WO6Xvvk1_cd">Trabaja
+                                sin dejar de estudiar</a></li>
+                        <li class="list-group-item"><a href="http://blog.occ.com.mx/los-7-trabajos-mas-contratados-en-el-2016/#.WO6ZNfnhDcc">Los
+                                7 trabajos más contratados del 2016</a></li>
+                        <li class="list-group-item"><a href="http://blog.occ.com.mx/marca-personal-en-la-vida-laboral/#.WO6ZXPnhDcc">
+                                Marca personal en la vida laboral</a></li>
+            </ul>
+            </div>
+          </div>
+        </div>
+      </div> 
+
+      <!--Info para llamar al servicio-->
+      <div class="card mx-auto mt-4" style="width: 50%;" id="infoservicio">
+        <div class="row">
+          <div class="col-md-2">
+            <img class="card-img mt-4 ml-5" src="../../assets/img/GaleriaServicios/logo.png" alt="Card image cap">
+          </div>
+          <div class="col-md-4">
+            <div class="card-body">
+              <h5 class="card-title">Contacto</h5>
+              <p class="card-text">Atención de lunes a viernes de 8:00 a 16:00 hrs.</p>
+            </div>
+          </div>
+          <div class="col-md-6">
+            <ul class="list-group list-group-flush" id="contactoinfo">
+              <li class="list-group-item"> <i class="fas fa-user"></i>Cristina Vázquez Reyes Sánchez </li>
+              <li class="list-group-item"><i class="fas fa-envelope"></i>cristinavazquez@utng.edu.mx</li>
+              <li class="list-group-item"><i class="fas fa-phone-square"></i>Tel:18 2 55 00 ext. 1357</li>
+            </ul>
+          </div>
+         
+        </div>
+       
+      </div>
+
+    </div>
+
+
+  </header>
+
+
+
+  <?php include '../Plantillas/footer.php';?>
+
+  <!-- Bootstrap core JS-->
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+  <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.bundle.min.js"></script>
+  <!-- Third party plugin JS-->
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery-easing/1.4.1/jquery.easing.min.js"></script>
+  <!-- Contact form JS-->
+  <script src="assets/mail/jqBootstrapValidation.js"></script>
+  <script src="assets/mail/contact_me.js"></script>
+  <!-- Core theme JS-->
+  <script src="js/scripts.js"></script>
+</body>
+
+</html>
