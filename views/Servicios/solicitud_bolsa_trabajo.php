6056cb359e026f165239f416cb2977a23d754f2c
Jose_Gabriel_Gonzalez <pepexglzpard@outlook.com>
2020-12-03 21:09:54 -0600

    Update solicitud_bolsa_trabajo.php

--- views/Servicios/solicitud_bolsa_trabajo.php
+++ views/Servicios/solicitud_bolsa_trabajo.php
@@ -13,119 +13,118 @@
   <script src="https://use.fontawesome.com/releases/v5.13.0/js/all.js" crossorigin="anonymous"></script>
   <!-- Google fonts-->
   <link href="https://fonts.googleapis.com/css?family=Montserrat:400,700" rel="stylesheet" type="text/css" />
-  <link href="https://fonts.googleapis.com/css?family=Lato:400,700,400italic,700italic" rel="stylesheet"
-    type="text/css" />
+  <link href="https://fonts.googleapis.com/css?family=Lato:400,700,400italic,700italic" rel="stylesheet" type="text/css" />
   <!-- Core theme CSS (includes Bootstrap)-->
   <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.5.1/jquery.min.js" integrity="sha512-bLT0Qm9VnAYZDflyKcBaQ2gg0hSYNQrJ8RilYldYQ1FxQYoCLtUjuuRuZo+fjqhx/qtq/1itJ0C2ejDxltZVFg==" crossorigin="anonymous"></script>
   <link href="../../css/styles.css" rel="stylesheet" />
   <link href="../../css/servicios.css" rel="stylesheet" />
   <link href="../../css/servicios/seguimiento.css" rel="stylesheet" />
   <script src="../../js/solicitud_bolsa_trabajo.js"> </script>
-<!-- JavaScript -->
-<script src="//cdn.jsdelivr.net/npm/alertifyjs@1.13.1/build/alertify.min.js"></script>
+  <!-- JavaScript -->
+  <script src="//cdn.jsdelivr.net/npm/alertifyjs@1.13.1/build/alertify.min.js"></script>
 
-<!-- CSS -->
-<link rel="stylesheet" href="//cdn.jsdelivr.net/npm/alertifyjs@1.13.1/build/css/alertify.min.css"/>
-<!-- Default theme -->
-<link rel="stylesheet" href="//cdn.jsdelivr.net/npm/alertifyjs@1.13.1/build/css/themes/default.min.css"/>
-<!-- Semantic UI theme -->
-<link rel="stylesheet" href="//cdn.jsdelivr.net/npm/alertifyjs@1.13.1/build/css/themes/semantic.min.css"/>
-<!-- Bootstrap theme -->
-<link rel="stylesheet" href="//cdn.jsdelivr.net/npm/alertifyjs@1.13.1/build/css/themes/bootstrap.min.css"/>
+  <!-- CSS -->
+  <link rel="stylesheet" href="//cdn.jsdelivr.net/npm/alertifyjs@1.13.1/build/css/alertify.min.css" />
+  <!-- Default theme -->
+  <link rel="stylesheet" href="//cdn.jsdelivr.net/npm/alertifyjs@1.13.1/build/css/themes/default.min.css" />
+  <!-- Semantic UI theme -->
+  <link rel="stylesheet" href="//cdn.jsdelivr.net/npm/alertifyjs@1.13.1/build/css/themes/semantic.min.css" />
+  <!-- Bootstrap theme -->
+  <link rel="stylesheet" href="//cdn.jsdelivr.net/npm/alertifyjs@1.13.1/build/css/themes/bootstrap.min.css" />
 
-<!-- 
+  <!-- 
     RTL version
 -->
-<link rel="stylesheet" href="//cdn.jsdelivr.net/npm/alertifyjs@1.13.1/build/css/alertify.rtl.min.css"/>
-<!-- Default theme -->
-<link rel="stylesheet" href="//cdn.jsdelivr.net/npm/alertifyjs@1.13.1/build/css/themes/default.rtl.min.css"/>
-<!-- Semantic UI theme -->
-<link rel="stylesheet" href="//cdn.jsdelivr.net/npm/alertifyjs@1.13.1/build/css/themes/semantic.rtl.min.css"/>
-<!-- Bootstrap theme -->
-<link rel="stylesheet" href="//cdn.jsdelivr.net/npm/alertifyjs@1.13.1/build/css/themes/bootstrap.rtl.min.css"/>
+  <link rel="stylesheet" href="//cdn.jsdelivr.net/npm/alertifyjs@1.13.1/build/css/alertify.rtl.min.css" />
+  <!-- Default theme -->
+  <link rel="stylesheet" href="//cdn.jsdelivr.net/npm/alertifyjs@1.13.1/build/css/themes/default.rtl.min.css" />
+  <!-- Semantic UI theme -->
+  <link rel="stylesheet" href="//cdn.jsdelivr.net/npm/alertifyjs@1.13.1/build/css/themes/semantic.rtl.min.css" />
+  <!-- Bootstrap theme -->
+  <link rel="stylesheet" href="//cdn.jsdelivr.net/npm/alertifyjs@1.13.1/build/css/themes/bootstrap.rtl.min.css" />
 </head>
 
 <body id="page-top">
   <!-- Navigation-->
 
-  <?php include '../Plantillas/header.php';?>
+  <?php include '../Plantillas/header.php'; ?>
 
   <!-- Masthead-->
   <header class="masthead text-white">
     <div class="container">
 
-      
 
-    <div class="card mx-auto mt-4">
+
+      <div class="card mx-auto mt-4">
         <div class="card-header" style="background-color:#5dbcd2;">
-            <div class="row mx-auto">
-             <h3 style="margin-left:30rem;">Solicitud de Servicio</h3>  
-            </div>
+          <div class="row mx-auto">
+            <h3 style="margin-left:30rem;">Solicitud de Servicio</h3>
+          </div>
         </div>
         <div class="card-body" style="background-color:#04AC84">
-                <form id="formValidar" method="POST" href="#">
-                    <div class="form-group">
-                        <label for="exampleInputEmail1">Nombre del alumno:</label>
-                        <input type="text" class="form-control" id="email" aria-describedby="emailHelp" name="nomAlumno" placeholder="Ingrese su nombre" required>
-                      
-                    </div>
-                    <div class="form-group">
-                        <label for="exampleInputEmail1">Correo Electronico:</label>
-                        <input type="email" class="form-control" id="email" aria-describedby="emailHelp" name="correo" placeholder="Ingrese correo electronico valido" required>
-                      
-                    </div>
-                    <div class="form-group">
-                        <label for="exampleInputPassword1">Numero de control</label>
-                        <input type="number" class="form-control" id="noControl" name="noControl" placeholder="No Control" required>
-                    </div>
-                    <div class="form-group">
-                        <label for="exampleInputPassword1">Egresado de la carrera:</label>
-                        <select class="form-control" id="exampleFormControlSelect1" name="nomCarrera">
-                            <option>Administración Área Capital Humano</option>
-                            <option>Desarrollo de Negocios Área Mercadotécnia</option>
-                            <option>Ingeniería en Redes Inteligentes y Ciberseguridad</option>
-                            <option>Ingeniería en Mecatrónica</option>
-                            <option>Ingeniería en Desarrollo y Gestión de Software</option>
-                            <option>Ingeniería en Tecnologías de la Producción</option>
-                            <option>Ingeniería en Entornos Virtuales y Negocios Digitales</option>
-                            <option>Licenciatura en Innovación de Negocios y Mercadotecnia</option>
-                            <option>Licenciatura en Contaduría</option>
-                            <option>TSU.Procesos Industriales Área Manufactura</option>
-                            <option>TSU.Infraestructura de Redes Digitales</option>
-                            <option>TSU.Desarrollo de Software Multiplataforma</option>
-                            <option>TSU.Energias Renovables Área Calidad y Ahorro de Energía</option>
-                            <option>TSU.Diseño Digital</option>
-                            <option>TSU.Mecatrónica Área Instalaciones Eléctricas Eficientes</option>
-                        </select>
-                    </div>
-                    <label for="exampleInput2">Tipo de servicio</label>
-                    <select class="form-control" id="tipoServicio" onchange="getval(this);" name="nomServicio">
-                        <option>Talleres para buscadores activos de empleo</option>
-                        <option>Revisión de CV Tip’s para búsqueda de empleo</option>
-                        <option>Bolsas de trabajo en línea</option>
-                        <option>Difusión de programas de apoyo al autoempleo</option>
-                        <option>Apoyo para la postulación de vacantes registradas en BT.</option>
-                    </select>
-                    <div class="form-group">
-                        <label for="exampleInputPassword1">Comentarios:</label>
-                        <textarea class="form-control" rows="3" placeholder="Añadir comentarios" ></textarea>
-                    </div>
-                    
+          <form id="formValidar" href="#" method="POST" >
+            <div class="form-group">
+              <label for="exampleInputEmail1">Nombre del alumno:</label>
+              <input type="text" class="form-control" id="email" aria-describedby="emailHelp" name="nomAlumno" placeholder="Ingrese su nombre" required>
 
-                    <div class="row" id="CargaArchivo"></div>
-                    
-                    <div class="center">
-                      <div class="row mx-auto">
-                      <input type="submit" name="enviar">
-                      </div>
-                    </div>
-                </form>
-                
-               <?php include("correo.php")?>
-  
+            </div>
+            <div class="form-group">
+              <label for="exampleInputEmail1">Correo Electronico:</label>
+              <input type="email" class="form-control" id="email" aria-describedby="emailHelp" name="correo" placeholder="Ingrese correo electronico valido" required>
+
+            </div>
+            <div class="form-group">
+              <label for="exampleInputPassword1">Numero de control</label>
+              <input type="number" class="form-control" id="noControl" name="noControl" placeholder="No Control" required>
+            </div>
+            <div class="form-group">
+              <label for="exampleInputPassword1">Egresado de la carrera:</label>
+              <select class="form-control" id="exampleFormControlSelect1" name="nomCarrera">
+                <option>Administración Área Capital Humano</option>
+                <option>Desarrollo de Negocios Área Mercadotécnia</option>
+                <option>Ingeniería en Redes Inteligentes y Ciberseguridad</option>
+                <option>Ingeniería en Mecatrónica</option>
+                <option>Ingeniería en Desarrollo y Gestión de Software</option>
+                <option>Ingeniería en Tecnologías de la Producción</option>
+                <option>Ingeniería en Entornos Virtuales y Negocios Digitales</option>
+                <option>Licenciatura en Innovación de Negocios y Mercadotecnia</option>
+                <option>Licenciatura en Contaduría</option>
+                <option>TSU.Procesos Industriales Área Manufactura</option>
+                <option>TSU.Infraestructura de Redes Digitales</option>
+                <option>TSU.Desarrollo de Software Multiplataforma</option>
+                <option>TSU.Energias Renovables Área Calidad y Ahorro de Energía</option>
+                <option>TSU.Diseño Digital</option>
+                <option>TSU.Mecatrónica Área Instalaciones Eléctricas Eficientes</option>
+              </select>
+            </div>
+            <label for="exampleInput2">Tipo de servicio</label>
+            <select class="form-control" id="tipoServicio" onchange="getval(this);" name="nomServicio">
+              <option>Talleres para buscadores activos de empleo</option>
+              <option>Revisión de CV Tip’s para búsqueda de empleo</option>
+              <option>Bolsas de trabajo en línea</option>
+              <option>Difusión de programas de apoyo al autoempleo</option>
+              <option>Apoyo para la postulación de vacantes registradas en BT.</option>
+            </select>
+            <div class="form-group">
+              <label for="exampleInputPassword1">Comentarios:</label>
+              <textarea class="form-control" rows="3" placeholder="Añadir comentarios"></textarea>
+            </div>
+
+
+            <div class="row" id="CargaArchivo"></div>
+
+            <div class="center">
+              <div class="row mx-auto">
+                <input type="submit" name="enviar">
+              </div>
+            </div>
+          </form>
+
+          <?php include("correo.php") ?>
+
         </div>
-    </div>
-   
+      </div>
+
 
       <!--Info para llamar al servicio-->
       <div class="card mx-auto mt-4" style="width: 50%;" id="infoservicio">
@@ -146,9 +145,9 @@
               <li class="list-group-item"><i class="fas fa-phone-square"></i>Tel:18 2 55 00 ext. 1357</li>
             </ul>
           </div>
-         
+
         </div>
-       
+
       </div>
 
     </div>
@@ -158,7 +157,7 @@
 
 
 
-  <?php include '../Plantillas/footer.php';?>
+  <?php include '../Plantillas/footer.php'; ?>
 
   <!-- Bootstrap core JS-->
   <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
