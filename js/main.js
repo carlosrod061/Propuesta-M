74a747121a12b9fcc1cc7b439f0a9a5d32a9cc23
Dani6M <dhaniele212@gmail.com>
2020-12-09 14:01:22 -0600

    Incubadora
    Servicios

---
+++ js/main.js
@@ -0,0 +1,9 @@
+const grid = new Muuri('.grid',{
+    layout: {        
+        rounding: false
+    }
+});
+
+window.addEventListener('load',() =>{
+    document.getElementById('grid').classList('imagenes-cargadas');
+});
